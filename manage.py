ad1f117f7098fe3933c1f237ec2350fc1ac25e2c
Oscar <shehuan320@163.com>
2018-02-06 18:42:34 +0800

    fix

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 
 from app import create_app, db
 from app.models import User, Role, Permission, Blog, Comment, Favourite, Label
@@ -18,10 +18,11 @@ def make_shell_context():
                 Permission=Permission)
 
 
-# 集成shell
+# 集成自定义shell命令
 # python manage.py {shell, db, test, runserver}
 manager.add_command('shell', Shell(make_context=make_shell_context))
-# 集成数据库迁移，步骤1执行一次即可，需要修改数据模型后需要迁移执行2、3步骤即可
+
+# 集成数据库迁移命令，步骤1执行一次即可，需要修改数据模型后需要迁移执行2、3步骤即可
 # 1.创建迁移仓库：python manage.py db init
 # 2.创建迁移脚本：python manage.py db migrate --message "initial migration"
 # 3.更新数据库：python manage.py db upgrade
